254efa8cf2221d8a8027dbdd6191be0739c4bced
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-07-19 13:36:53 -0500

    Added flexbox layout and additional css

--- scripts/pikomon.js
+++ scripts/pikomon.js
@@ -1,17 +1,17 @@
 export const pikomonCard = (pikomon) => {
     let html = `
     <div class="piko-card">
-        <div class="piko-img">
-            <img src=${pikomon.imageUrl} alt="An image of the pikomon: ${pikomon.name}">
-        </div>
         <div>
+            <img class="piko-img" src=${pikomon.imageUrl} alt="An image of the pikomon: ${pikomon.name}">
+        </div>
+        <div class="piko-info">
             <section class="piko-name">
                 <h3>${pikomon.name}</h3>
             </section>
-            <article class="piko-info info-text">
+            <article class="info-text">
                 <p>Pikodex Entry #${pikomon.id}</p>
                 <p class="piko-category">Category: ${pikomon.category}</p>
-                <p class="piko-abilities">Abilities: ${pikomon.abilities}p>
+                <p class="piko-abilities">Abilities: ${pikomon.abilities}<p>
                 <p class="piko-weakness">Weakness: ${pikomon.weakness}</p>
             </article>
             
